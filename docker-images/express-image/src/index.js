7f8a5defa4f18253a63bbf142364d318ab4fdb4f
K-do <miguel.dovalelopes@heig-vd.ch>
2021-05-21 15:38:10 +0200

    minor fixes

--- docker-images/express-image/src/index.js
+++ docker-images/express-image/src/index.js
@@ -4,40 +4,35 @@ var chance = new Chance();
 var express = require('express');
 var app = express();
 
-console.log("My favorite animal is the " + chance.animal());
-
 app.get('/', function(request, response) {
-response.send(generateAnimals());
+   response.send(generateAnimals());
 });
 
 app.listen(3000, function () {
-console.log('Accepting HTTP on port 3000!');
+   console.log('Accepting HTTP on port 3000!');
 });
 
 function generateAnimals() {
-var numberOfAnimals = chance.integer({
-min: 1,
-max: 10
-});
-console.log(numberOfAnimals);
-var animals = [];
-for (var i = 1; i < numberOfAnimals; i++) {
-var gender = chance.gender();
-var birthYear = chance.year({
-min: 2000,
-max: 2020
-});
-animals.push({
-animal: chance.animal(),
-name: chance.first({
-gender: gender
-}),
-gender: gender,
-birthday: chance.birthday({
-year: birthYear
-})
-});
-};
-console.log(animals);
-return animals;
+   var numberOfAnimals = chance.integer({
+      min: 1,
+      max: 5
+   });
+
+   console.log(numberOfAnimals);
+
+   var animals = [];
+   for (var i = 0; i < numberOfAnimals; i++) {
+      var gender = chance.gender();
+      var birthYear = chance.year({min: 2000,max: 2020});
+
+      animals.push({
+         animal: chance.animal(),
+         name: chance.first({gender: gender}),
+         gender: gender,
+         birthday: chance.birthday({year: birthYear})
+      });
+   };
+
+   console.log(animals);
+   return animals;
 }
