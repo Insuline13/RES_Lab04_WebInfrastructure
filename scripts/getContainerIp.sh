445d78eed77903a0f28c0b4b4a361c98199af87b
K-do <miguel.dovalelopes@heig-vd.ch>
2021-05-21 18:20:24 +0200

    add getId script

---
+++ scripts/getContainerIp.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# getContainerIp - get the container's ip provided as argument
+#
+# usage: getContainerIP <container_name>
+#
+# Miguel Do Vale Lopes 21.05.2021
+
+if [ $# -eq 0 ] 
+then
+  echo "Please provide a container's name"
+  exit 1
+fi
+
+docker inspect "$1" | grep "\"IPAddress\"" | head -n 1 | sed "s/\(\"\|,\| \)//g" | cut -d ':' -f 2
+exit 0
