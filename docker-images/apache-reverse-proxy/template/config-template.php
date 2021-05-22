632f478d25309daf91c38ddf87c296f4a8ab96e6
K-do <miguel.dovalelopes@heig-vd.ch>
2021-05-22 13:47:46 +0200

    add round-robin and sticksSession

--- docker-images/apache-reverse-proxy/template/config-template.php
+++ docker-images/apache-reverse-proxy/template/config-template.php
@@ -13,16 +13,19 @@
     <Proxy "balancer://dynamic">
 	    BalancerMember "http://<?php print "$dynamic_app_1"?>"
 	    BalancerMember "http://<?php print "$dynamic_app_2"?>"
-	</Proxy>
+	    ProxySet lbmethod=byrequests
+    </Proxy>
 
+    Header add Set-Cookie "ROUTEID=.%{BALANCER_WORKER_ROUTE}e; path=/" env=BALANCER_ROUTE_CHANGED
     <Proxy "balancer://static">
-	    BalancerMember "http://<?php print "$static_app_1"?>"
-	    BalancerMember "http://<?php print "$static_app_2"?>"
-	</Proxy>
+	    BalancerMember "http://<?php print "$static_app_1"?>" route=1
+	    BalancerMember "http://<?php print "$static_app_2"?>" route=2
+	    ProxySet stickysession=ROUTEID
+    </Proxy>
 
     ProxyPass "/api/animals/" "balancer://dynamic/"
     ProxyPassReverse "/api/animals/" 'balancer://dynamic/"
-      
+
     ProxyPass "/" "balancer://static/"
     ProxyPassReverse "/" "balancer://static/"
 
